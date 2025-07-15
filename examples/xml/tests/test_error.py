9d0bbd3893c3e08beda597278caa1d1d23497a82
Jakob Voss <voss@gbv.de>
2025-07-15 08:14:18 +0200

    Change definition of nested errors

--- examples/xml/tests/test_error.py
+++ examples/xml/tests/test_error.py
@@ -1,13 +1,37 @@
-from validation_error import ValidationError, ValidationWarning
+from validation_error import ValidationError, Locator
+
+def test_locator():
+    loc = Locator("offset", 0)
+    assert isinstance(loc, Locator)
+    assert str(loc) == "offset 0"
+    assert loc.to_dict() == { "format": "offset", "value": "0" }
+    assert loc.condense() == ("offset", "0")
+
+    loc = Locator("offset", 7, [ValidationError()])
+    assert loc.to_dict() == { "format": "offset", "value": "7", "errors": [ { } ] }
 
 def test_error():
     error = ValidationError()
-    assert error.message == "ValidationError"
+    assert not error.message
+    assert str(error) == "ValidationError"
+    assert error.to_dict() == { }
+    assert error.to_dict(level=True) == { "level": "error" }
 
-def test_validation_warning():
-    warning = ValidationError("", level="warning")
-    assert isinstance(warning, ValidationWarning)
-    assert warning.message == "ValidationWarning"
+    error = ValidationError("oops!")
+    assert str(error) == "ValidationError: oops!"
+    assert error.to_dict() == { "message": "oops!" }
 
-    warning = ValidationWarning()
-    assert warning.message == "ValidationWarning"
+    error = ValidationError("!")
+    assert error.message == "!"
+
+    error = ValidationError(message="!", position=[ Locator("char", "3") ])
+    assert error.to_dict() == { "message": "!", "position": { "char": "3" } }
+    assert error.to_dict(condense=False) == { "message": "!", "position": [ { "format": "char", "value": "3" } ] }
+
+    error = ValidationError(message="!", position= { "char": "3" })
+    assert error.to_dict() == { "message": "!", "position": { "char": "3" } }
+
+    error = ValidationError(position= [ { "format": "char", "value": "3" } ])
+    assert error.to_dict() == { "position": { "char": "3" } }
+
+# TODO: test nested errors
--- examples/xml/validation_error/__init__.py
+++ examples/xml/validation_error/__init__.py
@@ -1,47 +1,67 @@
-class ValidationError(Exception):
-    """An Error of Data Validation Error Format."""
-
-    def __new__(cls, message="", **fields):
-        """Instantiate ValidationError or ValidationWarning."""
-        level = ValidationWarning if fields.get("level") == "warning" else cls
-        if cls == level:
-            return Exception.__new__(cls, message, **fields)
-        else:
-            return level.__new__(level, message, **fields)
-
-    def __init__(self, message="", **fields):
-        self.message = message if message else self.__class__.__name__
-        if len(fields.get("types",[])):
-            self.types = fields["types"]
-        if fields.get("position"):
-            self.position = fields["position"]
-
-    def __str__(self):
-        # TODO: include position
-        return f"{self.message}"
-
-
-class ValidationWarning(ValidationError):
-    """An Error of Data Validation Error Format with level "warning"."""
-
-    def __init__(self, message="", **fields):
-        super().__init__(message, **fields)
-        self.level = "warning"
-
-
 class Locator:
-    """A Locator of Data Validation Error Format."""
+    """Location within a document, defined by locator format (aka dimension) and locator value."""
 
-    def __init__(self, format, value):
+    def __init__(self, format: str, value: str, errors=[]):
         self.format = format
-        self.value = value
+        self.value = str(value)
+        if not self.format or not self.value:
+            raise ValueError("Locator must have locator format and locator value!")
+        self.errors = [
+            e if isinstance(e, ValidationError) else ValidationError(e) for e in errors
+        ]
 
     def __str__(self):
         return f"{self.format} {self.value}"
 
+    def to_dict(self):
+        d = {"format": self.format, "value": self.value}
+        if self.errors:
+            d["errors"] = [e.to_dict() for e in self.errors]
+        return d
 
-class NestedLocator(Locator):
-    """A Nested Locator of Data Validation Error Format."""
+    def condense(self):
+        return (self.format, self.value)
 
-    # TODO
 
+class ValidationError(Exception):
+    """An Error of Data Validation Error Format."""
+
+    def __init__(self, message: str = "", **fields):
+        self.message = str(message) or fields.get("message", "")
+        self.types = fields.get("types", [])
+        self.level = fields.get("level", "error")
+        position = fields.get("position", [])
+        if type(position) == dict:  # expand condense form
+            position = [Locator(k, v) for (k, v) in position.items()]
+        else:
+            position = [
+                loc if isinstance(loc, Locator) else Locator(**loc) for loc in position
+            ]
+        self.position = position
+
+    def __str__(self):
+        cls = "ValidationError" if self.level == "error" else "ValidationWarning"
+        return f"{cls}: {self.message}" if self.message else cls
+
+    def to_dict(self, level=False, condense=True):
+        d = {}
+        if self.message:
+            d["message"] = self.message
+        if self.types:
+            d["types"] = self.types
+        if self.level and level:
+            d["level"] = self.level
+        if self.position:
+            position = self.position
+            if condense and condenseable(position):
+                d["position"] = dict([p.condense() for p in position])
+            else:
+                d["position"] = [p.to_dict() for p in position]
+        return d
+
+
+def condenseable(position):
+    if any([p.errors for p in position]):
+        return False
+    formats = [p.format for p in position]
+    return len(formats) == len(set(formats))
